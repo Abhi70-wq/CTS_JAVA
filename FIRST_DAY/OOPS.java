68891fdccc76d67a990e5d76399e0a4350867f61
abhishek <gabhishekyadav1@gmail.com>
2025-07-28 10:25:40 +0530

    Added OOPS.java

---
+++ FIRST_DAY/OOPS.java
@@ -0,0 +1,35 @@
+interface   Interface_oops{
+     void display1();
+}
+class INHERIT{
+    public void display(){
+        System.out.println("Inheritance");
+    }
+}
+class first extends INHERIT implements Interface_oops{
+    public void display(){
+        System.out.println(" inheritated class");
+    }
+    @Override
+    public void display1(){
+        System.out.println("Inheritance and abstraction");
+    }
+}
+class second implements Interface_oops{
+       @Override
+    public void display1(){
+        System.out.println("interface extends through @override");
+    }
+    
+}
+public class OOPS {
+    public static void main(String args[]){
+        INHERIT in=new INHERIT();
+        in.display();
+        first oo=new first();
+        oo.display();
+        oo.display1();
+        second sen =new second();
+        sen.display1();
+    }
+}
